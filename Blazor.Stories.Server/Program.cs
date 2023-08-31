076e9a4ffff37ada8bdec1d2c25ed93e4cf46730
Mariekie Coetzee <mariekie@mariekiecoetzee.com>
2023-08-31 13:23:32 +1000

    Add server

---
+++ Blazor.Stories.Server/Program.cs
@@ -0,0 +1,20 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+
+var app = builder.Build();
+
+
+
+app.UseHttpsRedirection();
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
